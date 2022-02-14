c84947ed5a82969bf811432d9bb0d92ae98fe6cd
mornki <1048515550@qq.com>
2022-02-14 18:24:05 +0800

    底部标签页切换和瞄点定位

--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 
 > vue 练习项目
 
+## 遇到的问题
+
+### 使用 mint-ui 首页加载样式闪烁
+
+描述：刷新页面会展现 1s 样式乱掉的页面
+原因： 项目中使用@import `@import "https://unpkg.com/mint-ui/lib/style.css"` 引入 mint-ui 样式，样式表style.css晚于 app.js 加载，出现样式渲染问题。
+解决方案：在 main.js中引入样式，会将样式文件前置，文件加载就应用样式文件。`import Mint from 'mint-ui'; import "mint-ui/lib/style.css"`
+
 ## 项目启动
 
 ```bash
@@ -33,17 +41,17 @@ For a detailed explanation on how things work, check out the [guide](http://vuej
 
 ## 模拟数据和代理请求
 
-参考：https://segmentfault.com/a/1190000012693158
+参考：<https://segmentfault.com/a/1190000012693158>
 
 ### json-server 模拟数据
 
 node>12
 
-1.  yarn add json-server
-2.  创建 db.json
-3.  启动 mock 服务
-    cd dbjson
-    json-server --watch --port 3001 db.json
+1. yarn add json-server
+2. 创建 db.json
+3. 启动 mock 服务
+   cd dbjson
+   json-server --watch --port 3001 db.json
 
 ### mock.js 模拟数据
 
@@ -143,23 +151,43 @@ nodemon.json
 
 ## 项目开发流程
 
-### 页面结构拆分
+### 页面拆分
 
 vue-router 的理解和使用
 router-view/router-link/keep-alive
 $router: 路由器对象, 包含一些操作路由的功能函数, 来实现编程式导航(跳转路由)
     $route: 当前路由对象, 一些当前路由信息数据的容器, path/meta/query/params
 
-1.组件拆分
+#### 组件拆分
 
 - 项目路由拆分
+
   - 底部导航组件: FooterGuide
   - 导航路由组件: Msite/Search/Order/Profile
   - 登陆路由组件: Login
+
 - 抽取组件
   - 头部组件: HeaderTop, 通过 slot 来实现组件通信标签结构
   - 商家列表组件: ShopList
-  - 标签页组件 FooterGuide 的显示/隐藏: 通过$router.replace("跳转到的路由")
+  - 标签页组件 FooterGuide 的显示/隐藏: 通过\$router.replace("跳转到的路由")
+
+#### 字体图标引入
+
+[阿里巴巴矢量图库]("https://www.iconfont.cn/?spm=a313x.7781069.1998910419.d4d0a486a")
+
+- 新建项目
+- 选择图标加入购物车
+- 购物车图标添加到新建项目中
+- 复制项目链接中图标地址到 vue 项目中的 index.html 中引入图标
+- 通过 `<i class="iconfont icon-search"></i>`使用
+
+#### UI 组件引入
+
+UI 组件
+
+- css component
+  - Header 顶部导航栏
+  - Tabbar 底部标签栏
 
 ### 标签页切换和选中功能（FooterGuide）
 
@@ -178,5 +206,17 @@ watch: {
 ```
 
 ### 头部组件通信（HeaderTop）
+
+slot 插槽
+
+```vue
+  <!-- 放置插槽 -->
+     <slot name="left"></slot>
+  <!-- 使用插槽 -->
+    <template slot="left"></template>
+    <template v-slot:left></template>
+```
+
+Vuex 组件间通信
 
 ### 登陆路由组件功能
--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,19 +1,16 @@
 <!--
  * @Date: 2022-02-10 16:57:36
  * @LastEditors: zhangwen
- * @LastEditTime: 2022-02-11 16:56:13
+ * @LastEditTime: 2022-02-14 18:22:24
  * @FilePath: /vue_practice_project/src/components/FooterGuide/FooterGuide.vue
 -->
 <template>
   <div>
-    <mt-tabbar v-model="selected" fixed>
-      <mt-tab-item id="home">首页</mt-tab-item>
-      <mt-tab-item id="search">
-        <i class="fas fa-search"></i>
-        搜索</mt-tab-item
-      >
-      <mt-tab-item id="order">订单</mt-tab-item>
-      <mt-tab-item id="profile">我的</mt-tab-item>
+    <mt-tabbar v-model="activedId" fixed>
+      <mt-tab-item v-for="v of tabbars" :key="v.id" :id="v.id">
+        <i :class="'iconfont icon-' + v.icon"></i>
+        <div>{{ v.title }}</div>
+      </mt-tab-item>
     </mt-tabbar>
   </div>
 </template>
@@ -23,18 +20,81 @@ export default {
   name: "FooterGuide",
   data() {
     return {
-      selected: this.$route.path.replace('/','') || "home"
+      activedId: "home",
+      tabbars: [
+        {
+          id: "home",
+          title: "首页",
+          icon: "shop"
+        },
+        {
+          id: "search",
+          title: "搜索",
+          icon: "search"
+        },
+        {
+          id: "order",
+          title: "订单",
+          icon: "filedone"
+        },
+        {
+          id: "profile",
+          title: "我的",
+          icon: "user"
+        }
+      ]
     };
   },
+  computed: {},
   watch: {
-    selected(){
-      console.log(this.$router)
-      this.$router.replace('/'+this.$data.selected);
+    /**
+     * 页签切换时，切换路由地址
+     */
+    // activedId(value){
+    //   this.changeUrl('/'+value);
+    // },
+    activedId: {
+      handler(value) {
+          this.changeUrl("/" + value);
+      },
+      immediate: true
+    },
+    /**
+     * 路由切换时更新页签
+     */
+    $route(to, from) {
+      let path = this.getPath(to.path);
+      if (path) {
+        this.$data.activedId = path;
+      }
     }
   },
-  methods: {},
-  mounted() {}
+  methods: {
+    // 根据路由，获取选中的页签id
+    getPath() {
+      let index = this.$data.tabbars.find(tab => {
+        return "/" + tab.id == this.$route.path;
+      });
+      // console.log("getPath",index.id)
+      return index ? index.id : "";
+    },
+    // 切换路由
+    changeUrl(path) {
+      // console.log("changeUrl",path);
+      console.log(this.$router);
+      // debugger;
+      this.$router.push({ path: path });
+    }
+  },
+  mounted() {
+    // console.log(this.$data);
+    // this.$data.activedId = this.getPath() || 'home';
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.iconfont {
+  font-size: 25px;
+}
+</style>
